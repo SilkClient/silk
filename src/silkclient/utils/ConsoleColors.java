2956041b0b801414bb94a65f4bed9ab516fe26e3
cainy-a <cainy-a@users.noreply.github.com>
2021-03-11 21:06:15 +0000

    Half-broken Hyperium injector

---
+++ src/silkclient/utils/ConsoleColors.java
@@ -0,0 +1,13 @@
+package silkclient.utils;
+
+public class ConsoleColors {
+	public static final String C_RESET = "\u001B[0m";
+	public static final String C_BLACK = "\u001B[30m";
+	public static final String C_RED = "\u001B[31m";
+	public static final String C_GREEN = "\u001B[32m";
+	public static final String C_YELLOW = "\u001B[33m";
+	public static final String C_BLUE = "\u001B[34m";
+	public static final String C_PURPLE = "\u001B[35m";
+	public static final String C_CYAN = "\u001B[36m";
+	public static final String C_WHITE = "\u001B[37m";
+}
