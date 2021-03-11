b111bf4a767a12f62860582ac07f2f6785ce18cf
cainy-a <cainy-a@users.noreply.github.com>
2021-03-11 19:16:39 +0000

    tabs > spaces

--- src/silkclient/utils/JarHandler.java
+++ src/silkclient/utils/JarHandler.java
@@ -15,152 +15,152 @@ import java.util.jar.JarOutputStream;
 
 public class JarHandler {
 
-    public void modifyJarFiles(File jarFile, String[] filesToRemove)  {
-        File tempFolder = new File(jarFile.getParentFile(), "temp/");
-        tempFolder.mkdirs();
+	public void modifyJarFiles(File jarFile, String[] filesToRemove)  {
+		File tempFolder = new File(jarFile.getParentFile(), "temp/");
+		tempFolder.mkdirs();
 
-        ZipUtil.unpack(jarFile, tempFolder);
+		ZipUtil.unpack(jarFile, tempFolder);
 
-        for (String file : filesToRemove) {
-            File f = new File(tempFolder, file);
-            if (f.isDirectory()) {
-                deleteDir(f);
-            } else {
-                if (f.exists()) {
-                    f.delete();
-                }
-            }
-        }
+		for (String file : filesToRemove) {
+			File f = new File(tempFolder, file);
+			if (f.isDirectory()) {
+				deleteDir(f);
+			} else {
+				if (f.exists()) {
+					f.delete();
+				}
+			}
+		}
 
-        try {
-            unpackHackClass(new File(tempFolder, Main.config.getProperty("INPUT_CLASS_HACK")));
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+		try {
+			unpackHackClass(new File(tempFolder, Main.config.getProperty("INPUT_CLASS_HACK")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-        jarFile.delete();
-        ZipUtil.pack(tempFolder, jarFile);
-        delete(tempFolder);
-    }
+		jarFile.delete();
+		ZipUtil.pack(tempFolder, jarFile);
+		delete(tempFolder);
+	}
 
-    private void delete(File element) {
-        if (element.isDirectory()) {
-            for(File f: element.listFiles()) {
-                this.delete(f);
-            }
-        }
-        element.delete();
-    }
+	private void delete(File element) {
+		if (element.isDirectory()) {
+			for(File f: element.listFiles()) {
+				this.delete(f);
+			}
+		}
+		element.delete();
+	}
 
-    private static void unpackHackClass(File toDir) throws IOException {
-        Files.copy(new File(".", Main.config.getProperty("INPUT_CLASS_HACK")).getAbsoluteFile().toPath(), toDir.getAbsoluteFile().toPath());
-    }
+	private static void unpackHackClass(File toDir) throws IOException {
+		Files.copy(new File(".", Main.config.getProperty("INPUT_CLASS_HACK")).getAbsoluteFile().toPath(), toDir.getAbsoluteFile().toPath());
+	}
 
-    public static boolean deleteDir(File directory) {
-        if(directory.exists()){
-            File[] files = directory.listFiles();
-            if(null!=files){
-                for(int i=0; i<files.length; i++) {
-                    if(files[i].isDirectory()) {
-                        deleteDir(files[i]);
-                    }
-                    else {
-                        files[i].delete();
-                    }
-                }
-            }
-        }
-        return(directory.delete());
-    }
+	public static boolean deleteDir(File directory) {
+		if(directory.exists()){
+			File[] files = directory.listFiles();
+			if(null!=files){
+				for(int i=0; i<files.length; i++) {
+					if(files[i].isDirectory()) {
+						deleteDir(files[i]);
+					}
+					else {
+						files[i].delete();
+					}
+				}
+			}
+		}
+		return(directory.delete());
+	}
 
-    public void replaceJarFile(String jarPathAndName,byte[] fileByteCode,String fileName) throws IOException {
-        File jarFile = new File(jarPathAndName);
-        File tempJarFile = new File(jarPathAndName + ".tmp");
-        JarFile jar = new JarFile(jarFile);
-        boolean jarWasUpdated = false;
+	public void replaceJarFile(String jarPathAndName,byte[] fileByteCode,String fileName) throws IOException {
+		File jarFile = new File(jarPathAndName);
+		File tempJarFile = new File(jarPathAndName + ".tmp");
+		JarFile jar = new JarFile(jarFile);
+		boolean jarWasUpdated = false;
 
-        try {
-            JarOutputStream tempJar = new JarOutputStream(new FileOutputStream(tempJarFile));
+		try {
+			JarOutputStream tempJar = new JarOutputStream(new FileOutputStream(tempJarFile));
 
-            // Allocate a buffer for reading entry data.
+			// Allocate a buffer for reading entry data.
 
-            byte[] buffer = new byte[1024];
-            int bytesRead;
+			byte[] buffer = new byte[1024];
+			int bytesRead;
 
-            try {
-                // Open the given file.
+			try {
+				// Open the given file.
 
-                try {
-                    // Create a jar entry and add it to the temp jar.
+				try {
+					// Create a jar entry and add it to the temp jar.
 
-                    JarEntry entry = new JarEntry(fileName);
-                    tempJar.putNextEntry(entry);
-                    tempJar.write(fileByteCode);
+					JarEntry entry = new JarEntry(fileName);
+					tempJar.putNextEntry(entry);
+					tempJar.write(fileByteCode);
 
-                } catch (Exception ex) {
-                    System.out.println(ex);
+				} catch (Exception ex) {
+					System.out.println(ex);
 
-                    // Add a stub entry here, so that the jar will close without an
-                    // exception.
+					// Add a stub entry here, so that the jar will close without an
+					// exception.
 
-                    tempJar.putNextEntry(new JarEntry("stub"));
-                }
-
-
-                // Loop through the jar entries and add them to the temp jar,
-                // skipping the entry that was added to the temp jar already.
-                InputStream entryStream = null;
-                for (Enumeration<JarEntry> entries = jar.entries(); entries.hasMoreElements(); ) {
-                    // Get the next entry.
-
-                    JarEntry entry = (JarEntry) entries.nextElement();
-
-                    // If the entry has not been added already, so add it.
-
-                    if (! entry.getName().equals(fileName)) {
-                        // Get an input stream for the entry.
-
-                        entryStream = jar.getInputStream(entry);
-                        tempJar.putNextEntry(entry);
-
-                        while ((bytesRead = entryStream.read(buffer)) != -1) {
-                            tempJar.write(buffer, 0, bytesRead);
-                        }
-                    }
-                }
-                if(entryStream!=null)
-                    entryStream.close();
-                jarWasUpdated = true;
-            }
-            catch (Exception ex) {
-                System.out.println(ex);
-
-                // IMportant so the jar will close without an
-                // exception.
-
-                tempJar.putNextEntry(new JarEntry("stub"));
-            }
-            finally {
-                tempJar.close();
-            }
-        }
-        finally {
-
-            jar.close();
-
-            if (! jarWasUpdated) {
-                tempJarFile.delete();
-            }
-        }
+					tempJar.putNextEntry(new JarEntry("stub"));
+				}
 
 
-        if (jarWasUpdated) {
-            if(jarFile.delete()){
-                tempJarFile.renameTo(jarFile);
-                System.out.println(jarPathAndName + " updated.");
-            }else
-                System.out.println("Could Not Delete JAR File");
-        }
-    }
+				// Loop through the jar entries and add them to the temp jar,
+				// skipping the entry that was added to the temp jar already.
+				InputStream entryStream = null;
+				for (Enumeration<JarEntry> entries = jar.entries(); entries.hasMoreElements(); ) {
+					// Get the next entry.
+
+					JarEntry entry = (JarEntry) entries.nextElement();
+
+					// If the entry has not been added already, so add it.
+
+					if (! entry.getName().equals(fileName)) {
+						// Get an input stream for the entry.
+
+						entryStream = jar.getInputStream(entry);
+						tempJar.putNextEntry(entry);
+
+						while ((bytesRead = entryStream.read(buffer)) != -1) {
+							tempJar.write(buffer, 0, bytesRead);
+						}
+					}
+				}
+				if(entryStream!=null)
+					entryStream.close();
+				jarWasUpdated = true;
+			}
+			catch (Exception ex) {
+				System.out.println(ex);
+
+				// IMportant so the jar will close without an
+				// exception.
+
+				tempJar.putNextEntry(new JarEntry("stub"));
+			}
+			finally {
+				tempJar.close();
+			}
+		}
+		finally {
+
+			jar.close();
+
+			if (! jarWasUpdated) {
+				tempJarFile.delete();
+			}
+		}
+
+
+		if (jarWasUpdated) {
+			if(jarFile.delete()){
+				tempJarFile.renameTo(jarFile);
+				System.out.println(jarPathAndName + " updated.");
+			}else
+				System.out.println("Could Not Delete JAR File");
+		}
+	}
 }
